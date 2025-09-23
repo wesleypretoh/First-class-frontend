0b948a0e24ff33c0e01cb3a5248151007059958f
chatthong <i.chatthong@gmail.com>
2025-09-24 01:08:45 +0700

    1.4.1 clean migration db

--- prisma/migrations/20250922191824_add_user_preferences/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."User" ADD COLUMN     "colorThemePreference" TEXT NOT NULL DEFAULT 'neutral',
-ADD COLUMN     "themePreference" TEXT NOT NULL DEFAULT 'system';
--- prisma/migrations/20250923212459_add_language_preference/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."User"
-    ADD COLUMN "languagePreference" TEXT NOT NULL DEFAULT 'en';
